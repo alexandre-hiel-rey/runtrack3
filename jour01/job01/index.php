0a898d6fd06c7090d4cc5bb020ba452f913b01a9
alexandre-hiel-rey <55872146+alexandre-hiel-rey@users.noreply.github.com>
2020-03-09 10:22:57 +0100

    Add files via upload

---
+++ jour01/job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Job 01</title>
+    <script>
+        alert('Hello Javascript!')
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
